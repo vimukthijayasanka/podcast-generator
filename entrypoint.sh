d66fb70dc8e55901ad23c140b2015ff0a79e1d98
Vimukthi Jayasanka <61610883+vimukthijayasanka@users.noreply.github.com>
2024-08-14 13:55:12 +0000

    Add DockerFile, actions.yaml, and entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /user/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+
+git push --set-upstream origin main
+
+echo "==================="
